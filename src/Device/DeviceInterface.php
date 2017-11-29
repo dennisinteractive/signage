c223e460e0f687afb9305f54a7a54331e1b1a5bf
Peter Clarke <peter_clarke@dennis.co.uk>
2017-11-29 12:10:09 +0000

    DeviceInterface

---
+++ src/Device/DeviceInterface.php
@@ -0,0 +1,44 @@
+<?php
+namespace Drupal\signage\Device;
+
+use Drupal\node\NodeInterface;
+
+interface DeviceInterface {
+
+  /**
+   * The id of the device.
+   *
+   * @return int
+   */
+  public function getId();
+
+  /**
+   * The device name.
+   *
+   * @return string
+   */
+  public function getName();
+
+  /**
+   * The drupal entity, as returned by node_load().
+   *
+   * @param $entity
+   *
+   * @return self
+   */
+  public function setNode(NodeInterface $entity);
+
+  /**
+   * The url the device should show if it has nothing else to show.
+   *
+   * @return string
+   */
+  public function getDefaultUrl();
+
+  /**
+   * The name of the channel the device is currently subscribed to.
+   * @return string
+   */
+  public function getChannelName();
+
+}
